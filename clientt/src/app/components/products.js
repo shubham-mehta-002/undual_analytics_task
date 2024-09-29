65668af3805c3df13c165bd7699b3b7a69407bf5
shubham-mehta-002 <134730928+shubham-mehta-002@users.noreply.github.com>
2024-09-29 17:26:33 +0530

    Update products.js

--- clientt/src/app/components/products.js
+++ clientt/src/app/components/products.js
@@ -26,8 +26,7 @@ export default function Products() {
         let queryString = "";
         if (searchCategories.length) {
           queryString = searchCategories
-            .map((category) => `category=${category}`)
-            .join("&");
+            .map((category) => `category=${category}&`)
         }
 
         if (searchInput.trim() !== "") {
