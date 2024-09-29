f77a20d9128136e5468483a3e4d7ee0e3457f410
shubham-mehta-002 <134730928+shubham-mehta-002@users.noreply.github.com>
2024-09-29 17:12:47 +0530

    Update products.js

--- clientt/src/app/components/products.js
+++ clientt/src/app/components/products.js
@@ -4,38 +4,41 @@ import { useEffect } from "react";
 import axios from "axios";
 import { BASE_URI } from "../constants/api";
 import { useAppDispatch, useAppSelector } from "../lib/hooks";
-import {
-  selectProducts,
-} from "../lib/store/features/product/productSlice";
+import { selectProducts } from "../lib/store/features/product/productSlice";
 import { setProducts } from "../lib/store/features/product/productSlice";
-import { selectPage, selectSearchCategories, selectSearchInput } from "../lib/store/features/filters/filtersSlice";
+import {
+  selectPage,
+  selectSearchCategories,
+  selectSearchInput,
+} from "../lib/store/features/filters/filtersSlice";
+import { selectProductFetchStatus } from "../lib/store/features/product/productSlice";
 import ProductCard from "./productCard";
-
 
 export default function Products() {
   const dispatch = useAppDispatch();
   const page = useAppSelector(selectPage) || 1;
-  const searchCategories = useAppSelector(selectSearchCategories)
-  const searchInput  =useAppSelector(selectSearchInput)
+  const searchCategories = useAppSelector(selectSearchCategories);
+  const searchInput = useAppSelector(selectSearchInput);
+
   useEffect(() => {
     const fetchProducts = async () => {
       try {
-        console.log([page]);
-        let queryString = '' 
-        if(searchCategories.length){
-            queryString = searchCategories.map(category => `category=${category}`).join('&');
+        let queryString = "";
+        if (searchCategories.length) {
+          queryString = searchCategories
+            .map((category) => `category=${category}`)
+            .join("&");
         }
 
-        if(searchInput.trim()!==''){
-          queryString += `search=${searchInput}&`
+        if (searchInput.trim() !== "") {
+          queryString += `search=${searchInput}&`;
         }
-        console.log({queryString})
-        queryString.slice(-1)
+
+        queryString.slice(-1);
         const response = await axios.get(
           `${BASE_URI}/products?page=${page}&limit=10&${queryString}`
         );
         if (response.data.success) {
-          console.log({ response: response.data });
           dispatch(setProducts(response.data));
         }
       } catch (error) {
@@ -44,14 +47,13 @@ export default function Products() {
     };
 
     const timeOut = setTimeout(() => {
-      fetchProducts(); // Call the async function
+      fetchProducts();
     }, 500);
 
-    return () => clearTimeout(timeOut); // Cleanup timeout on unmount or dependency change
-  }, [dispatch, page,searchCategories,searchInput]);
+    return () => clearTimeout(timeOut);
+  }, [dispatch, page, searchCategories, searchInput]);
 
   const products = useAppSelector(selectProducts);
-  console.log({ products });
 
   return (
     <div className="wrapper w-[100%] lg:w-[70%] ">
@@ -62,8 +64,8 @@ export default function Products() {
       ) : (
         <div className="products-container w-full grid grid-cols-2 md:grid-cols-3  ">
           {products?.map((product, index) => (
-            <div ley={index} className="flex items-center justify-center mb-2">
-              <ProductCard key={product.id} {...product} />
+            <div key={index} className="flex items-center justify-evenly mb-2">
+              <ProductCard {...product} />
             </div>
           ))}
         </div>
